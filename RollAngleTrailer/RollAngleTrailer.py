38fcc6ca1938842bfea6fb3de0ec0d3a644c0744
Jason Moore <moorepants@gmail.com>
2011-02-07 16:12:35 -0800

    the results for the pitch angle don't seem to change

--- RollAngleTrailer/RollAngleTrailer.py
+++ RollAngleTrailer/RollAngleTrailer.py
@@ -8,27 +8,49 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import pi
 from scipy.optimize import fsolve
+import svgfig as svg
 
 # define some bicycle roll angles
 roll = np.linspace(-pi/3, pi/3, num=500)
 
 # the geometry
 rr = 13.
-rt = 2.
-d1 = 13.
-d2 = 0.
-d3 = 0.
-d4 = 15.
+rt = 4.
+d1 = 2.
+d2 = 2.
+d3 = 8.
+d4 = 16.
 d5 = 6.
 
-def find_pitch_pot(x, roll, rr, rt, d1, d2, d3, d4, d5):
+# draw the wheels
+rwheel = svg.SVG("circle", cx=0., cy=-rr, r=rr)
+twheel = svg.SVG("circle", cx=d2+d4, cy=-rt, r=rt)
+# draw the trailer hitch
+hitch1 = svg.SVG('line', x1=0., y1=-rr, x2=0., y2=-(rr-d1))
+hitch2 = svg.SVG('line', x1=0., y1=-(rr-d1), x2=d2, y2=-(rr-d1))
+hitch = svg.SVG('g', hitch1, hitch2)
+# draw the trailer
+tr = []
+tr.append(svg.SVG("line", x1=d2, y1=-(rr-d1), x2=d2, y2=-(rr-d1-d3)))
+tr.append(svg.SVG("line", x1=d2, y1=-(rr-d1-d3), x2=d2+d4, y2=-(rr-d1-d3)))
+tr.append(svg.SVG('line', x1=d2+d4, y1=-(rr-d1-d3), x2=d2+d4, y2=-(rt)))
+trailer = svg.SVG('g', tr[0], tr[1], tr[2])
+# add some dots for the joints
+j1 = svg.SVG('circle', cx=d2, cy=-(rr-d1), r=0.5)
+j2 = svg.SVG('circle', cx=d2+d4, cy=-rt, r=0.5)
+j = svg.SVG('g', j1, j2, stroke='blue', fill='blue')
+
+group = svg.SVG("g", rwheel, twheel, hitch, trailer, j,
+                transform="translate(20, 50)")
+group.save()
+group.inkview()
+
+def find_pitch(x, roll, rr, rt, d1, d2, d3, d4, d5):
     '''
-    x[0] : pitch
-    x[1] : pot
+    x : pitch
     '''
     eq = np.zeros(2)
-    eq[0] = rt - np.cos(roll)*(rr-d1-d4*np.sin(x[0])-(rr-d1-rt)*np.cos(x[0]))
-    eq[1] = np.sin(roll)*np.cos(x[1]) + np.sin(x[1])*np.cos(x[0])*np.cos(roll)
+    eq[0] = rt - np.cos(roll)*(rr-d1-d4*np.sin(x)-(rr-d1-rt)*np.cos(x))
     return eq
 
 pitch = np.zeros_like(roll)
@@ -37,29 +59,21 @@ pot = np.zeros_like(roll)
 for i, ang in enumerate(roll):
     if i == 0:
         pitchguess = 0.
-        potguess = 0.
     else:
         pitchguess = pitch[i-1]
-        potguess = pot[i-1]
-    sol = fsolve(find_pitch_pot,
-                 [pitchguess, potguess],
+    pitch[i] = fsolve(find_pitch, 0.,
                  args=(ang, rr, rt, d1, d2, d3, d4, d5))
-    pitch[i] = sol[0];
-    pot[i] = -np.tan(roll)/np.cos(pitch)
-    pot[i] = sol[1];
+    pot[i] = np.arctan(-np.tan(ang)/np.cos(pitch[i]))
 
+# plot the potentiometer angle
 plt.figure(1)
-# calculate the potentiometer angle
 plt.plot(np.rad2deg(roll), np.rad2deg(pot))
-plt.figure(2)
-# calculate the trailer fork pitch
-plt.plot(np.rad2deg(roll), np.rad2deg(pitch))
-
-plt.figure(1)
 plt.xlabel('Bicycle roll angle')
 plt.ylabel('Potentiometer angle')
 
+# plot the trailer fork pitch
 plt.figure(2)
+plt.plot(np.rad2deg(roll), np.rad2deg(pitch))
 plt.xlabel('Bicycle roll angle')
 plt.ylabel('Trailer pitch angle')
 

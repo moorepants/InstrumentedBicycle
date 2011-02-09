10cf341c3166279de852e956236c079d44f78f2a
Jason Moore <moorepants@gmail.com>
2011-02-09 11:10:21 -0800

    seems like fsolve was not converging on the solution closest to the guessed root, so i changed to the newton function

--- RollAngleTrailer/RollAngleTrailer.py
+++ RollAngleTrailer/RollAngleTrailer.py
@@ -7,60 +7,58 @@ shows the trailer pitch angle as a function of bicycle frame roll.
 import matplotlib.pyplot as plt
 import numpy as np
 from math import pi
-from scipy.optimize import fsolve
+from scipy.optimize import fsolve, newton
 import svgfig as svg
 
-def pitch_roll_constraint(x, roll, rr, rt, d1, d4):
-    '''
+def pitch_roll_constraint(x, roll, rr, rt, d1, d4): '''
     x : trailer pitch
     '''
     return rt - np.cos(roll)*(rr-d1-d4*np.sin(x)-(rr-d1-rt)*np.cos(x))
 
 def compute_pitch(pitchguess, roll, rr, rt, d1, d4):
-    return fsolve(pitch_roll_constraint, pitchguess, args=(roll, rr, rt, d1, d4))
+    return newton(pitch_roll_constraint, pitchguess, args=(roll, rr, rt, d1, d4))
 
 def compute_pot(roll, pitch):
     return np.arctan(-np.tan(roll)/np.cos(pitch))
 
-def draw_trailer(roll, rr, rt, d1, d2, d3, d4, d5):
-    # calculate the pitch and pot
-    pitch = compute_pitch(0., roll, rr, rt, d1, d4)
-    pot = compute_pot(roll, pitch)
-
-    # some common expressions
-    sr = np.sin(roll)
-    cr = np.cos(roll)
-    sp = np.sin(pitch)
-    cp = np.cos(pitch)
-    spo = np.sin(pot)
-    cpo = np.cos(pot)
-
-    # draw the bicycle wheel
-    bwcen = [0., rr*cr]
-    bwrad = [rr, rr*cr]
-    bwheel = svg.SVG('ellipse', cx=bwcen[0], cy=bwcen[1], rx=bwrad[0],
-            ry=bwrad[1])
-
-    # draw the right trailer wheel
-    rwcen = 
-    (0.5*d5*sp*spo+(rr-d1-rt)*sp-d2-d4*cp)*n1> + (d4*sp*cr+(d1-rr)*cr+(rr-d1-rt)*cp*cr+0.5*d5*(sr*cpo+spo*cp*cr))*n3>
-    rwheel = svg.SVG('circle', cx=rwcen[0], cy=rwcen[1], r=rt)
-
-    scene = svg.SVG('g', bwheel, rwheel)
-    scene.save('scene.svg')
-    scene.inkview()
+###def draw_trailer(roll, rr, rt, d1, d2, d3, d4, d5):
+    #### calculate the pitch and pot
+    ###pitch = compute_pitch(0., roll, rr, rt, d1, d4)
+    ###pot = compute_pot(roll, pitch)
+###
+    #### some common expressions
+    ###sr = np.sin(roll)
+    ###cr = np.cos(roll)
+    ###sp = np.sin(pitch)
+    ###cp = np.cos(pitch)
+    ###spo = np.sin(pot)
+    ###cpo = np.cos(pot)
+###
+    #### draw the bicycle wheel
+    ###bwcen = [0., rr*cr]
+    ###bwrad = [rr, rr*cr]
+    ###bwheel = svg.SVG('ellipse', cx=bwcen[0], cy=bwcen[1], rx=bwrad[0],
+            ###ry=bwrad[1])
+###
+    #### draw the right trailer wheel
+    ###rwcen = 
+    ###(0.5*d5*sp*spo+(rr-d1-rt)*sp-d2-d4*cp)*n1> + (d4*sp*cr+(d1-rr)*cr+(rr-d1-rt)*cp*cr+0.5*d5*(sr*cpo+spo*cp*cr))*n3>
+    ###rwheel = svg.SVG('circle', cx=rwcen[0], cy=rwcen[1], r=rt)
+###
+    ###scene = svg.SVG('g', bwheel, rwheel)
+    ###scene.save('scene.svg')
+    ###scene.inkview()
 
 # define some bicycle roll angles
-roll = np.linspace(-pi/3, pi/3, num=500)
+roll = np.linspace(-pi/2, pi/2, num=500)
 
 # the geometry
 rr = 13.
-d1 = 12
-rt = rr-d1
+d1 = 0.
+rt = 1.5
 d2 = 0.
-d3 = 0.
-d4 = 16.
-d5 = 6.
+d3 = 12.
+d3 = 0.  d5 = 6.
 
 pitch = np.zeros_like(roll)
 pot = np.zeros_like(roll)
@@ -70,7 +68,9 @@ for i, ang in enumerate(roll):
         pitchguess = 0.
     else:
         pitchguess = pitch[i-1]
-    pitch[i] = compute_pitch(pitchguess, ang, rr, rt, d1, d4)
+    x = compute_pitch(pitchguess, ang, rr, rt, d1, d4)
+    print x
+    pitch[i] = x
     pot[i] = compute_pot(ang, pitch[i])
 
 # plot the potentiometer angle
@@ -84,6 +84,10 @@ plt.figure(2)
 plt.plot(np.rad2deg(roll), np.rad2deg(pitch))
 plt.xlabel('Bicycle roll angle')
 plt.ylabel('Trailer pitch angle')
+
+plt.figure(3)
+p = np.linspace(-pi, pi, num=100)
+plt.plot(p, pitch_roll_constraint(p, 0., rr, rt, d1, d4))
 
 plt.show()
 
@@ -110,4 +114,4 @@ group.save()
 
 group.inkview()
 
-draw_trailer(pi/3, rr, rt, d1, d2, d3, d4, d5)
+#draw_trailer(pi/3, rr, rt, d1, d2, d3, d4, d5)

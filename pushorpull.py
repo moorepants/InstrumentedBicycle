b9a6ea2ddb0ef73fff57634ea6dfbdefb9804b71
Jason Moore <moorepants@gmail.com>
2011-08-29 22:53:45 -0700

    added a little program for randomly choosing a push or pull

---
+++ pushorpull.py
@@ -0,0 +1,17 @@
+import random
+import time
+
+print "Press enter when the rider is stable or type q and press enter to quit"
+
+command = None
+
+while command != 'q':
+    fromKeyboard = raw_input()
+    if fromKeyboard == 'q':
+        command = fromKeyboard
+    else:
+        print "wait..."
+        command = random.choice([True, False])
+        commands = ['Push', 'Pull']
+        time.sleep(random.uniform(0., 10.))
+        print commands[command]
